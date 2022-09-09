a87e2387be831a91f557ecc5a69efbdb7209e670
nidarg <gradin_g@yahoo.com>
2022-09-09 22:14:30 +0300

    gg

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from './Navbar'
 import { Outlet } from 'react-router-dom'
 
+// Outlet component in the Layout component inserts the matching child route
 const Layout = () => {
   return (
     <div>
       <Navbar/>
       <Outlet/>
+      
     </div>
   )
 }
